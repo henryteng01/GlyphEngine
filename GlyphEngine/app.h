664372b1957fa81c32c716f072d8924efe96789d
heteng <heteng@umich.edu>
2023-08-18 00:38:08 +0100

    created app struct

---
+++ GlyphEngine/app.h
@@ -0,0 +1,18 @@
+//
+//  app.h
+//  GlyphEngine
+//
+//  Created by Henry Teng on 8/18/23.
+//
+
+#ifndef app_h
+#define app_h
+
+#include <SDL2/SDL_video.h>
+
+struct App {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+#endif /* app_h */
--- GlyphEngine/main.cpp
+++ GlyphEngine/main.cpp
@@ -7,10 +7,12 @@
 
 #include <iostream>
 #include <SDL2/SDL.h>
+#include "app.h"
 
 int main(int argc, const char * argv[]) {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
     }
+    std::cout << "Hello, World!" << std::endl;
     return EXIT_SUCCESS;
 }
