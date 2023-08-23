c22f951175fae6d7d92f89d16461da85457b6bd9
heteng <heteng@umich.edu>
2023-08-23 21:13:27 +0100

    shrunk pacman

--- GlyphEngine/draw.cpp
+++ GlyphEngine/draw.cpp
@@ -23,8 +23,6 @@ SDL_Texture *loadTexture(SDL_Renderer *renderer, char* filename) {
     
     SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
     
-    
-    
     texture = IMG_LoadTexture(renderer, filename);
 
     if (!texture) {
@@ -41,6 +39,9 @@ void blit(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y) {
     dest.x = x;
     dest.y = y;
     SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+    
+    dest.w = 25;
+    dest.h = 25;
     
     SDL_RenderCopy(renderer, texture, NULL, &dest);
 }
